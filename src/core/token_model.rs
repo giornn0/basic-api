dac6ad261789a6354679794642b4a7a8840a74ce
giornn0 <shaaamsuu@gmail.com>
2022-07-18 09:07:54 -0300

    added JWT basic functionality,working without need of hit the db

--- src/core/token_model.rs
+++ src/core/token_model.rs
@@ -1,30 +1,84 @@
 use std::sync::Arc;
 
-use serde::{Serialize, Deserialize};
+use chrono::Utc;
+use jsonwebtoken::{
+    decode, encode, errors::Error as JWTError, DecodingKey, EncodingKey, Header, Validation, TokenData,
+    Algorithm
+};
+use serde::{Deserialize, Serialize};
 
-use crate::core::{server_model::Pool,errors::Error};
+use crate::{
+    core::{errors::Error, server_model::Pool},
+    utils::server::token_key,
+};
 
+#[derive(Serialize,Deserialize,Debug,Clone)]
+pub enum Role{
+    Admin,
+    User,
+    Client
+}
 
-pub trait FromToken{
-    fn from_token(  token: String, db_pool: Arc<Pool>)->Result<Self, Error> where Self: Sized{
+pub trait FromToken {
+    fn decode(&self, token: String)->Result<TokenData<Self>, Error> where Self: Sized;
+    fn from_token(token: String, db_pool: Arc<Pool>) -> Result<Self, Error>
+    where
+        Self: Sized,
+    {
         Err(Error::WrongToken)
     }
 }
 
-
-#[derive(Serialize, Deserialize, Debug,Clone)]
-pub struct AuthPayload{
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct AuthPayload {
     id: i32,
     model: String,
-    expiration: String,
+    name: String,
+    role: Role,
+    exp: i64,
 }
-impl Default for AuthPayload{
+impl Default for AuthPayload {
     fn default() -> Self {
-        AuthPayload { id: 4, model: "testing".to_owned(), expiration: "12/05/2022 15:50".to_owned() }
+        let expiration = Utc::now()
+            .checked_add_signed(chrono::Duration::seconds(60))
+            .expect("valid timestamp")
+            .timestamp();
+        AuthPayload {
+            id: 4,
+            model: "testing".to_owned(),
+            name: "naming".to_owned(),
+            role: Role::Client,
+            exp: expiration,
+        }
     }
 }
-impl FromToken for AuthPayload{
-    fn from_token(token: String, db_pool: Arc<Pool>)-> Result<AuthPayload, Error> {
-       Ok(AuthPayload::default())
+impl FromToken for AuthPayload {
+    fn decode(&self, token: String)->Result<TokenData<Self>, Error> where Self: Sized{
+        decode::<AuthPayload>(
+            &token,
+            &DecodingKey::from_secret(token_key().as_bytes()),
+            &Validation::new(Algorithm::HS256)).map_err(reject_error)
     }
+    fn from_token(token: String, db_pool: Arc<Pool>) -> Result<AuthPayload, Error> {
+        let test = AuthPayload::default();
+
+        let token1 = encode_model(&test)?;
+        let decoded = test.decode(token1)?;
+        println!("{:?}", &decoded.claims);
+        Ok(test)
+    }
+}
+
+fn reject_error(err: JWTError) -> Error {
+    println!("{}", err);
+    Error::WrongToken
+}
+
+fn encode_model<T: Serialize>(model: &T) -> Result<String, Error> {
+    encode(
+        &Header::new(Algorithm::HS256),
+        model,
+        &EncodingKey::from_secret(token_key().as_bytes()),
+    )
+    .map_err(reject_error)
 }
--- src/utils/server.rs
+++ src/utils/server.rs
@@ -1,8 +1,11 @@
 use std::num::ParseIntError;
 
-pub fn port()->Result<u16, ParseIntError>{
+pub fn port() -> Result<u16, ParseIntError> {
     std::env::var("PORT")
-      .ok()
-      .map(|val| val.parse::<u16>())
-      .unwrap_or(Ok(8080))
+        .ok()
+        .map(|val| val.parse::<u16>())
+        .unwrap_or(Ok(8080))
+}
+pub fn token_key() -> String {
+    std::env::var("JWT_KEY").ok().unwrap_or("without_secret".to_owned())
 }
